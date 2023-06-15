256c84d45fd36a783142cef1ed1fc65d135c972b
Jakub-Pazio <19jakubpazio99@gmail.com>
2023-06-15 23:45:30 +0200

    Initial file system and serving mock page

--- webapp/index.js
+++ webapp/index.js
@@ -2,9 +2,17 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// Serve static files from the "public" directory
+app.use(express.static('public'));
+
+// Define a route to serve the HTML page
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.sendFile('index.html', { root: 'public' });
+});
+
+app.get('/action', (req, res) => {
+  res.send({ cosik: 'XD' });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
